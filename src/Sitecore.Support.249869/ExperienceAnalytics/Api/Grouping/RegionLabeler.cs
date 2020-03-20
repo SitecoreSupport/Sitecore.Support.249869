c0c606158128cdd5ce107e648676f6247ccd4675
Sergey Plashenko <sergey.plashenko@sitecore.com>
2020-03-20 16:40:27 +0200

    Code from 9.0.1

---
+++ src/Sitecore.Support.249869/ExperienceAnalytics/Api/Grouping/RegionLabeler.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using Sitecore.Diagnostics;
+using Sitecore.ExperienceAnalytics.Api;
+using Sitecore.ExperienceAnalytics.Core.Diagnostics;
+using Sitecore.ExperienceAnalytics.Core.Grouping;
+using Sitecore.Globalization;
+using Sitecore.StringExtensions;
+
+namespace Sitecore.Support.ExperienceAnalytics.Api.Grouping
+{
+    internal class RegionLabeler : IVisitGroupLabeler
+    {
+        private readonly ILogger _logger;
+
+        public RegionLabeler(ILogger logger)
+        {
+            _logger = logger;
+        }
+
+        public RegionLabeler()
+            : this(ApiContainer.GetLogger())
+        {
+        }
+
+        public string GetLabel(string groupId, Language language)
+        {
+            Assert.IsNotNullOrEmpty(groupId, "key");
+            try
+            {
+                return (groupId == "ZZ") ? "[unknown region]" : GetRegionTranslation(groupId);
+            }
+            catch (Exception)
+            {
+                _logger.Warn("Could not resolve dimension key for region: '{0}'".FormatWith(groupId));
+                return "[unknown region]";
+            }
+        }
+
+        private static string GetRegionTranslation(string regionCode)
+        {
+            return typeof(Regions).GetField(regionCode).GetValue(null) as string;
+        }
+    }
+}
---
+++ src/Sitecore.Support.249869/ExperienceAnalytics/Api/Response/DimensionKeyTransformers/RegionDimensionKeyTransformer.cs
@@ -0,0 +1,20 @@
+﻿using Sitecore.ExperienceAnalytics.Api.Response.DimensionKeyTransformers;
+using Sitecore.ExperienceAnalytics.Core.Grouping;
+using Sitecore.Globalization;
+using Sitecore.Support.ExperienceAnalytics.Api.Grouping;
+
+namespace Sitecore.Support.ExperienceAnalytics.Api.Response.DimensionKeyTransformers
+{
+    public class RegionDimensionKeyTransformer : IDimensionKeyTransformer
+    {
+        private readonly IVisitGroupLabeler _regionGroupLabeler = new RegionLabeler();
+
+        public string UnknownLabel => "[unknown region]";
+
+        public string Transform(string key, Language language)
+        {
+            return Translate.Text(_regionGroupLabeler.GetLabel(key, language));
+        }
+    }
+
+}
