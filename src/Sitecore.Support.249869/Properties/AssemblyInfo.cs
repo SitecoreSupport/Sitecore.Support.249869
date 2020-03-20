a86bd78bb12a025e5d5ae8a47090d7507f0796d0
Sergey Plashenko <sergey.plashenko@sitecore.com>
2020-03-20 16:40:08 +0200

    Autogenerated data.

---
+++ src/Sitecore.Support.249869/Properties/AssemblyInfo.cs
@@ -0,0 +1,6 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Sitecore.Support.249869")]
+[assembly: AssemblyProduct("Sitecore.Support.249869")]
+[assembly: ComVisible(false)]
